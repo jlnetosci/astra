52daff67f8adc27dee479d865b574e91dfe7604e
jlnetosci <jlnetosci@gmail.com>
2024-03-27 22:37:29 +0000

    update FAQs 2024-03-27

--- pages/faq.py
+++ pages/faq.py
@@ -27,21 +27,37 @@ def add_logo():
         unsafe_allow_html=True,
     )
 
-def show():
-    st.title(r"\textbf{FAQ Page}")  # Use LaTeX for bold title
+def faq():
+    st.header(r"$\textbf{\textsf{FAQ Page}}$")  
+    
+    expand_all = st.toggle("Expand all", value=False)
 
     # FAQ data
     faq_data = {
-        "Question 1": "Answer 1",
-        "Question 2": "Answer 2",
-        "Question 3": "Answer 3",
-        # Add more questions and answers as needed
+        'What is ASTRA GEDCOM Viewer?': 'ASTRA is a web app that displays a "star map" like network visualization of a family tree organized as GEDCOM file.',
+
+        'What is a GEDCOM?': 'GEDCOM is the shorthand for <b><u>Ge</b></u>nealogical <b><u>D</b></u>ata <b><u>Com</b></u>munication. GEDCOM files are the standard to store and share genealogical data. While the format was developed by the Church of Jesus Christ of Latter-day Saints, it is an open standard used by most genealogical software. <p> \
+        More information on GEDCOM: <a href="https://en.wikipedia.org/wiki/GEDCOM" target="_blank" rel="noopener noreferrer">Wikipedia</a>; <a href="https://www.familysearch.org/en/wiki/GEDCOM" target="_blank" rel="noopener noreferrer">Familysearch</a>.',
+
+        'How do I get a GEDCOM file?': 'The usage of this web app assumes you already have already started your genealogical research and are using a web tool or genealogical software to build your family tree. <p> \
+        GEDCOM files are usually exported with a .ged extension. Depending on your prefered genealogical software there may be different steps in obtaining the file. Most tools have instructions on how to export the family tree as GEDCOM \
+        (for example <a href="https://gramps-project.org/gramps-manual/2.2/en/ch03s05.html" target="_blank">Gramps</a>, <a href="https://www.myheritage.com/help-center?a=How-do-I-download-(export)-a-GEDCOM-file-of-my-family-tree-from-my-family-site---id--bPSFTnHBQNauRgs6JzqGiA" target="_blank">MyHeritage</a>, or <a href="https://en.geneanet.org/help/how-to-export-your-geneanet-family-tree" target="_blank">Geneanet</a>).',
+        
+        'Can I export a GEDCOM file from FamilySearch?': 'As of the date of writing this FAQ (March 2024), FamilySearch’s documentation states: “Currently, a GEDCOM file cannot be exported directly from FamilySearch Family Tree.”',
+
+        'Will I be able to upload my family tree in other formats into ASTRA?': 'Currently, ASTRA solely parses GEDCOM files, if you have any suggestions regarding other formats please contact me with your suggestion.',
+
+        'Does ASTRA keep my GEDCOM data?': 'Short answer: No. <p>\
+        Longer answer: The GEDCOM file is read into a temporary file and deleted immediately after its parsing. Furthermore, the app is deployed Streamlit Cloud where data is ephemeral and files are removed upon changing the state of the app (i.e. refreshing) or closing the browser tab.',
+
+        'What should I do if I encounter an error?': 'Please contact me.'
+
     }
 
     # Display expandable boxes for each question-answer pair
     for question, answer in faq_data.items():
-        with st.expander(r"\textbf{" + question + r"}"):  # Use LaTeX for bold label
-            st.write(answer)
+        with st.expander(r"$\textbf{\textsf{" + question + r"}}$", expanded=expand_all):  # Use LaTeX for bold label
+            st.markdown(f'<div style="text-align: justify;"> {answer} </div>', unsafe_allow_html=True)
 
 # Streamlit configuration
 st.set_page_config(layout="centered")
@@ -49,5 +65,5 @@ st.set_page_config(layout="centered")
 # Show logo above navigation bar
 add_logo()
 
-# Show FAQ.
-show()
+#Show FAQ.
+faq()
