33e81b9ee944650ec93742be3dfa824600e56ca3
jlnetosci <jlnetosci@gmail.com>
2024-04-17 17:14:39 +0100

    Update FAQ to match website. Better formating.

--- pages/faq.py
+++ pages/faq.py
@@ -38,20 +38,20 @@ def faq():
 
     # FAQ data
     faq_data = {
-        'What is ASTRAviewer?': 'ASTRAviewer is a web app that displays a "star map" like network visualization of a family tree organized as GEDCOM file.',
+        'What is ASTRAviewer?': '<p>ASTRAviewer is a web app that displays a "star map" like network visualization from family trees stored as GEDCOM files.',
 
-        'What is a GEDCOM?': 'GEDCOM is the shorthand for <b><u>Ge</b></u>nealogical <b><u>D</b></u>ata <b><u>Com</b></u>munication. GEDCOM files are the standard to store and share genealogical data. While the format was developed by the Church of Jesus Christ of Latter-day Saints, it is an open standard used by most genealogical software. <p> \
-        More information on GEDCOM: <a href="https://en.wikipedia.org/wiki/GEDCOM" target="_blank" rel="noopener noreferrer">Wikipedia</a>; <a href="https://www.familysearch.org/en/wiki/GEDCOM" target="_blank" rel="noopener noreferrer">Familysearch</a>.',
+        'What is a GEDCOM?': 'GEDCOM is the shorthand for <b><u>Ge</b></u>nealogical <b><u>D</b></u>ata <b><u>Com</b></u>munication. GEDCOM files are the standard to store and share genealogical data. While the format was developed by the Church of Jesus Christ of Latter-day Saints, it is an open standard used by most genealogical software. \
+        <p><br>More information on GEDCOM: <a href="https://en.wikipedia.org/wiki/GEDCOM" target="_blank" rel="noopener noreferrer">Wikipedia</a>; <a href="https://www.familysearch.org/en/wiki/GEDCOM" target="_blank" rel="noopener noreferrer">Familysearch</a>.</p>',
 
-        'How do I get a GEDCOM file?': 'The usage of this web app assumes you already have already started your genealogical research and are using a web tool or genealogical software to build your family tree. <p> \
-        GEDCOM files are usually exported with a .ged extension. Depending on your prefered genealogical software there may be different steps in obtaining the file. Most tools have instructions on how to export the family tree as GEDCOM \
-        (for example <a href="https://gramps-project.org/gramps-manual/2.2/en/ch03s05.html" target="_blank">Gramps</a>, <a href="https://www.myheritage.com/help-center?a=How-do-I-download-(export)-a-GEDCOM-file-of-my-family-tree-from-my-family-site---id--bPSFTnHBQNauRgs6JzqGiA" target="_blank">MyHeritage</a>, or <a href="https://en.geneanet.org/help/how-to-export-your-geneanet-family-tree" target="_blank">Geneanet</a>).',
-        
-        'Can I export a GEDCOM file from FamilySearch?': 'As of the date of writing this FAQ (March 2024), FamilySearch’s documentation states: “Currently, a GEDCOM file cannot be exported directly from FamilySearch Family Tree.”',
+        'How do I get a GEDCOM file?': 'GEDCOM files are usually exported with a .ged extension. Depending on your prefered genealogical software there may be different steps in obtaining the file. Most tools have instructions on how to export the family tree as GEDCOM. \
+        (for example <a href="https://gramps-project.org/gramps-manual/2.2/en/ch03s05.html" target="_blank">Gramps</a>, <a href="https://www.myheritage.com/help-center?a=How-do-I-download-(export)-a-GEDCOM-file-of-my-family-tree-from-my-family-site---id--bPSFTnHBQNauRgs6JzqGiA" target="_blank">MyHeritage</a>, or <a href="https://en.geneanet.org/help/how-to-export-your-geneanet-family-tree" target="_blank">Geneanet</a>). \
+        <p><br>The usage of this web app assumes you already have already started your genealogical research and are using a web tool or genealogical software to build your family tree. If you have not, there are companies that will do this research for you. Unfortunately, at this time I cannot suggest any. If you are a genealogical research company and want to discuss a partnership with us please reach out.</p>',
 
-        'Will I be able to upload my family tree in other formats into ASTRAviewer?': 'Currently, ASTRAviewer solely parses GEDCOM files, if you have any suggestions regarding other formats please contact me with your suggestion.',
+        'Can I export a GEDCOM file from FamilySearch?': '<p>As of the date of writing this FAQ (March 2024), FamilySearch’s documentation states: “Currently, a GEDCOM file cannot be exported directly from FamilySearch Family Tree.”</p>',
 
-        'What should I do if I encounter an error?': 'Please contact me.'
+        'Will I be able to upload my family tree in other formats into ASTRAviewer?': '<p>Currently, ASTRAviewer solely parses GEDCOM files, if you have any suggestions regarding other formats please contact me with your suggestion.</p>',
+
+        'What should I do if I encounter an error?': '<p>Please contact me.</p>'
 
     }
 
